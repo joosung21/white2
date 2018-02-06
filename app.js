0af70c72210af59a435bab3155f2ad55608b12f6
Joosung Ahn <joosung21@gmail.com>
2018-02-06 15:30:44 +0900

    Complete Member

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ app.use('/', express.static('public'))
 app.use('/assets/', express.static('assets'))
 
 app.get('/', function (req, res){res.render('index')})
+app.get('/login', function (req, res){res.render('login')})
 app.get('/view', function (req, res){res.render('view')})
 app.get('/book', function (req, res){res.render('book')})
 app.get('/confirm', function (req, res){res.render('confirm')})
 app.get('/favorite', function (req, res){res.render('favorite')})
 app.get('/book-list', function (req, res){res.render('book-list')})
+app.get('/member', function (req, res){res.render('member')})
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
